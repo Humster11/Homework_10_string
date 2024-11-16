c00b6f7dc07462416e3b5070394ac7896e78a638
daria.lapina <ms.humster11@gmail.com>
2024-11-16 15:44:18 +0300

    Выполненное ДЗ

---
+++ src/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + newFullName);
+    }
+}
